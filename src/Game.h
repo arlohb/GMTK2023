dee7ebcc9b7b0dc5de6ce45d1520025a5ac45792
arlohb <arlo@urwick.co.uk>
2023-07-08 08:48:16 +0100

    Cleaned up Game.h

--- src/Game.h
+++ src/Game.h
@@ -8,30 +8,24 @@ class Game {
     public:
         Game(int width, int height, rl::Window& window);
 
-        /// Clamp all the meters between 0 and 100
-        void ClampMeters();
-        /// Draw the meters to the screen
-        void DrawMeters();
-
-        /// This set currentEvent to a new event
-        void NewEvent();
-        /// This draws the currentEvent if it exists
-        void DrawEvent();
-        void ApplyEventOption(EventOption& option);
-
-        bool isRunning = true;
-        std::string endMsg;
-        void DrawEnd();
-        void CheckEndGame();
-
-        /// Returns true if game exited
-        bool Loop();
+        ///////////////////////////////
+        ///         Window          ///
+        ///////////////////////////////
 
         int width;
         int height;
         rl::Window& window;
 
-        /// All these are between 0 and 100
+        bool isRunning = true;
+
+        /// Returns true if game exited
+        bool Loop();
+
+        ///////////////////////////////
+        ///         Meters          ///
+        ///////////////////////////////
+
+        // All these are between 0 and 100
         const int metersCount = 5;
         float workerHappiness = 50;
         float productivity = 50;
@@ -39,9 +33,30 @@ class Game {
         float boardConfidence = 50;
         float money = 50;
 
+        void ClampMeters();
+        void DrawMeters();
+
+        ///////////////////////////////
+        ///         Events          ///
+        ///////////////////////////////
+
         std::vector<Event> events;
 
         // Can't use a ref here
         std::optional<Event*> currentEvent;
+
+        /// This set currentEvent to a new event
+        void NewEvent();
+        /// This draws the currentEvent if it exists
+        void DrawEvent();
+        void ApplyEventOption(EventOption& option);
+
+        ///////////////////////////////
+        ///        End game         ///
+        ///////////////////////////////
+
+        std::string endMsg;
+        void DrawEnd();
+        void CheckEndGame();
 };
 
