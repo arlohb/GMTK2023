9699181e78dc53930d7cbae485c7c42d942fff3b
arlohb <arlo@urwick.co.uk>
2023-07-09 09:18:32 +0100

    Added loads of events

--- src/Events.cpp
+++ src/Events.cpp
@@ -138,6 +138,81 @@ Events::Events() {
                 {"Just fire them, that's enough damage.", 15, 0, 0, -10, 0},
             }
         ),
+        Event(
+            "Some employees were sharing their salaries with each other,\n"
+            "And they found some were paid more than others at the same level.\n"
+            "More often than not, the women are paid less than men in equal roles.\n"
+            "You didn't decide on their pay, but you could fix your predecessor's mistakes.",
+            {
+                {"Bring up everyone's pay in line with each other.", 15, 5, 0, 0, -10},
+                {"Reduce everyone's pay to make everyone equal.", -15, -5, 0, 0, 10},
+                {"Do nothing.", -10, 0, 0, 0, 0},
+            }
+        ),
+        Event(
+            "The previous boss promised the paperclip prices would stay static for the next 5 years.\n"
+            "In hindsight that was a stupid promise, and you've got to fix his mistake.\n"
+            "Prices of all the raw materials and factory running costs are going up.\n"
+            "Do you raise the prices?",
+            {
+                {"I didn't make the promise, we can't afford this.", 0, 0, -10, -5, 15},
+                {"A promise is a promise, we can't disapoint all our customers.", 0, 0, 10, 5, -15},
+            }
+        ),
+        Event(
+            "The average rent in the office's area is increasing,\n"
+            "And employees are complaining about struggling to afford rent.\n"
+            "Other companies are raising wages,\n"
+            "And the board is woried we'll lose some paperclip manufacture talent.",
+            {
+                {"Raise the wages.", 10, 0, 0, 5, -10},
+                {"Do nothing.", -5, 0, 0, -5, 0},
+            }
+        ),
+        Event(
+            "A study was done into various paperclip manufacturers,\n"
+            "And our paperclips had a 10% failure rate.\n"
+            "The board are saying this is just unnacceptable.\n"
+            "You could commit to raising quality, but people are gonna buy your paperclips anyway.",
+            {
+                {"Launch an internal paperclip quality investigation.", -5, -5, 20, 5, -10},
+                {"Do nothing, people will soon forget this study.", 0, 0, -10, -5, 0},
+            }
+        ),
+        Event(
+            "One of our factories overseas was found to be relying on child labour.\n"
+            "The press made a big thing of this, but every other paperclip company does this.\n"
+            "You could become the better company here, and stop this.\n"
+            "Or you could dilute the negative press against you...",
+            {
+                {"Stop this. Child labour is unnacceptable.", 0, 0, 15, 0, -10},
+                {"Pay for some articles uncovering other companies' child labour in paperclip manufacture.", 0, 0, -5, 0, -5},
+                {"Do nothing, the press will move on.", 0, 0, -15, -5, -5},
+            }
+        ),
+        Event(
+            "Some of the board is suggesting we are out of touch with the next generation of paperclip enjoyers.\n"
+            "Somebody brought up YouTube or TikTok as a solution to this.\n"
+            "Plenty of companies have big success on these platforms,\n"
+            "And this could solidify your position in the market.",
+            {
+                {"Create a TikTok account in the style of many small businesses.", 0, 0, 15, 10, 10},
+                {"Create a corporate feeling YouTube account.", 0, 0, -5, 15, 5},
+                {"Collaborate with a well known tech YouTuber giving them a tour.", 0, 0, 10, 10, 5},
+                {"Do nothing, we don't need social media to survive.", 0, 0, 0, -5, -5},
+            }
+        ),
+        Event(
+            "A few recent batches of paperclips had a 90% failure rate.\n"
+            "We can contact the customers who recieved this batch and refund them.\n"
+            "There is a legal loophole that means we don't necessarily *have* to.\n"
+            "And this would cost a significant amount to sort out.",
+            {
+                {"Refund them, we have a duty to supply working paperclips.", 0, 0, 20, 0, -15},
+                {"Don't reach out, but if any companies contact us refund them.", 0, 0, 10, 0, -5},
+                {"Do nothing, why do more than what we legally have to?", 0, 0, -10, 0, 0},
+            }
+        ),
     };
 
     eventsInDay = events.size();
